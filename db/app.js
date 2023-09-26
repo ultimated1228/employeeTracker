91c6a9d4b87af6c37ec810c232bd784756646584
ultimated1228 <stevenlucasmeyer@gmail.com>
2023-09-26 13:38:40 -0600

    built out apps.js file.  Completely untested.  At the least still need to connect the database, and figure out how to reference manager, roles, and departments within the inquirier prompts.

---
+++ db/app.js
@@ -0,0 +1,307 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const consoleTable = require('console.table');
+
+const {
+    viewAllDepartments,
+    viewAllRoles,
+    viewAllEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole
+} = require('./yourCrudFunctions');
+
+//build the main menu functions
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit',
+                ],
+            },
+        ])
+        .then((answer) => {
+            switch (answer.choice) {
+                case 'View all departments':
+                    //need to build remove comment when done
+                    viewAllDepartments();
+                    break;
+
+                case 'View all roles':
+                    //need to build remove comment when done
+                    viewAllRoles();
+                    break;
+
+                case 'View all employees':
+                    //need to build remove comment when done
+                    viewAllEmployees();
+                    break;
+
+                case 'Add a department':
+                    //need to build remove comment when done
+                    addDepartment();
+                    break;
+
+                case 'Add a role':
+                    //need to build remove comment when done
+                    addRole();
+                    break;
+
+                case 'Add an employee':
+                    //need to build remove comment when done
+                    addEmployee();
+                    break;
+
+                case 'Update an employee role':
+                    //need to build remove comment when done
+                    updateEmployeeRole();
+                    break;
+
+                case 'Exit':
+                    console.log('Exiting the application.');
+                    process.exit();
+
+                default:
+                    console.log('Invalid choice. Please try again.');
+                    mainMenu();
+            }
+        });
+}
+//recall the main menu
+mainMenu();
+
+
+// Create connection to mysql database
+const connection = mysql.createConnection({
+    //go back and look at assignment files to complete these correctly
+    host: '',
+    user: 'root',
+    password: '',
+    database: '',
+});
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database.');
+});
+
+//CREATE THE VARIOUS FUNCTIONS
+
+// Function to view all departments
+function viewAllDepartments() {
+    const query = 'SELECT * FROM departments';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\nAll Departments:\n');
+        console.table(res);
+
+        mainMenu();
+    });
+}
+
+// Function to view all roles
+function viewAllRoles() {
+    const query = 'SELECT * FROM roles';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\nAll Roles:\n');
+        console.table(res);
+
+        mainMenu();
+    });
+}
+
+// Function to view all employees
+function viewAllEmployees() {
+    const query = 'SELECT * FROM employees';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\nAll Employees:\n');
+        console.table(res);
+
+        mainMenu();
+    });
+}
+
+// Function to add a new department
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter the name of the new department:',
+            },
+        ])
+        .then((answer) => {
+            const { name } = answer;
+
+            // Insert the new department into the database
+            const query = 'INSERT INTO departments (name) VALUES (?)';
+            connection.query(query, [name], (err, res) => {
+                if (err) {
+                    console.error('Error adding department:', err);
+                } else {
+                    console.log('Department added successfully!');
+                }
+
+                mainMenu();
+            });
+        });
+}
+
+
+// Function to add a new role
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Enter the title of the new role:',
+            },
+            {
+                type: 'number',
+                name: 'salary',
+                message: 'Enter the salary for the new role:',
+            },
+            {
+                type: 'list',
+                name: 'departmentId',
+                message: 'Select the department for the new role:',
+                choices: [/* Populate this array with department options from your database */],
+            },
+        ])
+        .then((answers) => {
+            const { title, salary, departmentId } = answers;
+
+            // Insert the new role into the database
+            const query = 'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)';
+            connection.query(query, [title, salary, departmentId], (err, res) => {
+                if (err) {
+                    console.error('Error adding role:', err);
+                } else {
+                    console.log('Role added successfully!');
+                }
+
+                mainMenu();
+            });
+        });
+}
+
+
+// Function to add an employee
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: "Enter the employee's first name:",
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: "Enter the employee's last name:",
+            },
+            {
+                type: 'list',
+                name: 'roleId',
+                message: "Select the employee's role:",
+                choices: [/* reference role options from the database.  Need to figure out how to do this. */],
+            },
+            {
+                type: 'list',
+                name: 'managerId',
+                message: "Select the employee's manager:",
+                choices: [/* reference role options from the database.  Need to figure out how to do this. */],
+            },
+        ])
+        .then((answers) => {
+            const { firstName, lastName, roleId, managerId } = answers;
+
+            // Insert the new employee into the database
+            const query = 'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+            connection.query(query, [firstName, lastName, roleId, managerId], (err, res) => {
+                if (err) {
+                    console.error('Error adding employee:', err);
+                } else {
+                    console.log('Employee added successfully!');
+                }
+
+                mainMenu();
+            });
+        });
+}
+
+
+// Function to update an employee's role
+function updateEmployeeRole() {
+    // Query the database for list of employees currently in the database
+    const employeeQuery = 'SELECT id, CONCAT(first_name, " ", last_name) AS employee_name FROM employees';
+
+    connection.query(employeeQuery, (err, employees) => {
+        if (err) {
+            console.error('Error querying employees:', err);
+            mainMenu();
+            return;
+        }
+
+        inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'employeeId',
+                    message: 'Select the employee to update:',
+                    choices: employees.map((employee) => ({
+                        name: employee.employee_name,
+                        value: employee.id,
+                    })),
+                },
+                {
+                    type: 'list',
+                    name: 'newRoleId',
+                    message: 'Select the new role for the employee:',
+                    choices: [/* reference role options from the database.  Need to figure out how to do this. */],
+                },
+                {
+                    type: 'list',
+                    name: 'newManagerId',
+                    message: 'Select the new manager for the employee:',
+                    choices: employees.map((employee) => ({
+                        name: employee.employee_name,
+                        value: employee.id,
+                    })),
+                },
+            ])
+            .then((answers) => {
+                const { employeeId, newRoleId } = answers;
+
+                // Update the employee's role in the database
+                const updateQuery = 'UPDATE employees SET role_id = ? WHERE id = ?';
+                connection.query(updateQuery, [newRoleId, employeeId], (err, res) => {
+                    if (err) {
+                        console.error('Error updating employee role:', err);
+                    } else {
+                        console.log('Employee role updated successfully!');
+                    }
+
+                    mainMenu();
+                });
+            });
+    });
+}
+
+module.exports = { mainMenu };
+
